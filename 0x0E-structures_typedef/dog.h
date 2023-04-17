edb13d3dffd26e6816d14eda807bd8ebdcaa4e5f
Aya560 <ayahashad81@gmail.com>
2023-04-17 13:48:36 -0700

    e

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct for a dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: struct called "dog" with name, age and owner as its elements.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
