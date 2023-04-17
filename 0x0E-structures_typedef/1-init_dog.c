86db5c478afe70137dd5fea91a600d772db11b8b
Aya560 <ayahashad81@gmail.com>
2023-04-17 15:08:23 -0700

    My first commit

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,18 +1,18 @@
-#include <stdlib.h>
-#include "dog.h"
-
 /**
-   * init_dog - initializes a struct dog
-    * @d: pointer to struct dog to initialize
-     * @name: name to initialize the dog with
-      * @age: age to initialize the dog with
-       * @owner: owner to initialize the dog with
-*/
+ * init_dog - initializes a struct dog
+ * @d: pointer to struct dog to initialize
+ * @name: name to set in dog struct
+ * @age: age to set in dog struct
+ * @owner: owner to set in dog struct
+ *
+ * Return: void
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-if (d == NULL)
-d = malloc(sizeof(struct dog));
-d->name = name;
-d->age = age;
-d->owner = owner;
+	if (d == NULL)
+	return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
