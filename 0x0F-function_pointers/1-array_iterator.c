69ad8b6c607035f46cedcd4dd20d3de8512d7e91
Aya560 <ayahashad81@gmail.com>
2023-04-19 13:27:31 -0700

    My first commit

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,20 +1,19 @@
-#include <stddef.h>
-
-/*
- * array_iterator - Applies a function to each element of an array
- * @array: The array to iterate through
- * @size: The size of the array
- * @action: The function to apply to each element of the array
- *
- * Return: void
- */
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+   * array_iterator - prints each array append
+    * @array: array
+     * @size: how many append to print
+      * @action: pointer to print in hex
+       * Return: void
+*/
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i;
-
-	if (!array || !action)
-		return;
-
-	for (i = 0; i < size; i++)
-		action(array[i]);
+unsigned int i;
+if (array == NULL || action == NULL)
+return;
+for (i = 0; i < size; i++)
+{
+action(array[i]);
+}
 }
